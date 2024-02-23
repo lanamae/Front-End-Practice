b41eab7d4e489b18e2bb5409d9d7c259b5b51245
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2024-02-23 21:46:39 +0800

    added the index.js

--- RESPONSIVE WEBSITE/index.js
+++ RESPONSIVE WEBSITE/index.js
@@ -1,3 +1,16 @@
 const navlinks = document.querySelector('.navlinks');
 const menuBar = document.querySelector('.menu-bar')
-const close = document.querySelector('.close')
+const close = document.querySelector('.close');
+
+
+const showNav = () =>{
+   navlinks.classList.add('show')
+}
+
+const hideNav = () =>{
+    navlinks.classList.remove('show')
+ }
+
+
+menuBar.addEventListener('click', showNav)
+close.addEventListener('click', hideNav)
